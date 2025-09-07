201c89b9df4182819fe810f63d50b943f0caf2d6
guerillacodester <guerillacodester@gmail.com>
2025-09-07 18:43:23 +0100

    wip

--- api/main.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-E:\espressif\v5.4.1\python_env\idf5.4_py3.11_env\Scripts\python.exe "%~dp0main.py" %*
--- test_countries.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Testing Fleet Countries API...
-curl -s http://localhost:8000/fleet/countries
-echo.
-pause
